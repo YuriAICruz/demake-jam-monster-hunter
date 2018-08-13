a2e851b2805ca6caf1f49b5d24c8868860eb5259
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-08-13 14:40:56 -0300

    init, movement and basic hud

---
+++ Assets/Scripts/ActionBeat/Animation/AnimationController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityEngine;
+
+namespace ActionBeat.Animation
+{
+    public class AnimationController
+    {
+        private readonly Transform _transform;
+        private readonly Animator _animator;
+        private Vector3 _lastPos;
+        private int _lastDir;
+
+        public AnimationController(Transform transform, Animator animator)
+        {
+            _transform = transform;
+            _animator = animator;
+
+            _lastPos = _transform.position;
+        }
+
+        int GetDir(Vector2 velocity)
+        {
+            var dir = 0;
+            if (velocity.magnitude == 0)
+            {
+                return _lastDir;
+            }
+
+            if (velocity.x > velocity.y)
+            {
+                if (velocity.x > 0)
+                    dir = 2;
+                else
+                    dir = 4;
+            }
+            else
+            {
+                if (velocity.y > 0)
+                    dir = 1;
+                else
+                    dir = 3;
+            }
+            _lastDir = dir;
+            return dir;
+        }
+
+        public void SetVelocity(Vector2 velocity)
+        {
+            velocity *= 100;
+
+            var dir = GetDir(velocity);
+            
+            _animator.SetFloat("Speed", velocity.magnitude);
+
+            if (velocity.magnitude == 0)
+            {
+                switch (dir)
+                {
+                    case 1:
+                        velocity = new Vector2(0, 1);
+                        break;
+                    case 2:
+                        velocity = new Vector2(1, 0);
+                        break;
+                    case 3:
+                        velocity = new Vector2(-1, 0);
+                        break;
+                    case 4:
+                        velocity = new Vector2(0, -1);
+                        break;
+                }
+            }
+
+            if (velocity.x < 0)
+                _transform.localScale = new Vector3(-1, 1, 1);
+            else
+                _transform.localScale = new Vector3(1, 1, 1);
+
+            _animator.SetFloat("X", velocity.x);
+            _animator.SetFloat("Y", velocity.y);
+        }
+    }
+}
---
+++ Assets/Scripts/ActionBeat/ZeldaLikeCharacter.cs
@@ -0,0 +1,68 @@
+﻿using ActionBeat.Animation;
+using Physics;
+using Shooter;
+using Shooter.InputManage;
+using UnityEngine;
+
+namespace ActionBeat
+{
+    public class ZeldaLikeCharacter : MonoBehaviour
+    {
+        public ZeldaLikePhysics Physics;
+        private AnimationController _animationController;
+        public Life Life;
+        public Stamina Stamina;
+
+        private ZeldaLikeInputDispatcher InputDispatcher;
+
+        private void Awake()
+        {
+            Life.Reset();
+            Stamina.Reset();
+        }
+
+        void Start()
+        {
+            _animationController = new AnimationController(transform, GetComponent<Animator>());
+            
+            Physics.SetPosition(transform.position);
+            Physics.SetCollider(GetComponent<Collider2D>());
+        }
+
+        private void OnEnable()
+        {
+            if (InputDispatcher == null)
+                InputDispatcher = new ZeldaLikeInputDispatcher(this);
+
+            InputDispatcher.Attack += Attack;
+            InputDispatcher.Deffend += Deffend;
+            InputDispatcher.LeftStick += Move;
+        }
+
+        private void OnDisable()
+        {
+            InputDispatcher.Attack -= Attack;
+            InputDispatcher.Deffend -= Deffend;
+            InputDispatcher.LeftStick -= Move;
+        }
+
+        private void Move(Vector2 dir)
+        {
+            Physics.Move(dir);
+            _animationController.SetVelocity(Physics.Velocity);
+            transform.position = Physics.Position;
+        }
+
+        private void Deffend()
+        {
+            Stamina.DoAction(1);
+            // throw new System.NotImplementedException();
+        }
+
+        private void Attack()
+        {
+            Stamina.DoAction(5);
+            // throw new System.NotImplementedException();
+        }
+    }
+}
