6c3c958c74cd4e15f8ded1c152ff008440b7ed6a
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-08-23 22:41:14 -0300

    level and menu

---
+++ Assets/Scripts/ActionBeat/Presentation/Duration.cs
@@ -0,0 +1,32 @@
+﻿using UiGenerics;
+using UnityEngine;
+
+namespace ActionBeat.Presentation
+{
+    public class Duration : TextView
+    {
+        private ActionGameManagement _manager;
+        private float _startTime;
+
+        void Setup()
+        {
+        }
+
+        private void Start()
+        {
+            _manager = FindObjectOfType<ActionGameManagement>();
+
+            _manager.StartGame += ResetTime;
+        }
+
+        private void ResetTime()
+        {
+            _startTime = Time.time;
+        }
+
+        private void Update()
+        {
+            Text.text = (Time.time - _startTime).ToString("00.00");
+        }
+    }
+}
